bc14a550d1e9abc18a6197ffe807a45d82926de5
eliseubaldo <eliseu_baldo@hotmail.com>
2016-05-16 11:42:31 -0400

    flex positions

--- app/controllers/textCtrl.js
+++ app/controllers/textCtrl.js
@@ -1,4 +1,16 @@
-app.controller('textCtrl', function($scope){
+app.controller('textCtrl', function($scope, $http){
 
+	$scope.items = [];
+
+	$scope.getthem = function(){
+		
+		$http.get('http://jsonplaceholder.typicode.com/posts')
+		.then(function(response){
+			$scope.items = response.data;
+		});
+
+	}
+
+	
 
 });
